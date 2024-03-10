e4767184e108c707dae549d08e180b7e767ec2cb
Ekaluk52003 <ekaluk.p@gmail.com>
2024-03-10 16:55:51 +0700

    finished

--- nextjs/src/app/home/page.tsx
+++ nextjs/src/app/home/page.tsx
@@ -1,21 +1,20 @@
-// import { cookies } from "next/headers";
 "use server";
 import React, { useState } from "react";
-import { cookies } from "next/headers";
 import { logout, getSession } from "../action";
-
-
 
 export default async function Home() {
   const data = await getSession();
 
-
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Who {data.email}
+      <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4'>
+        This page is protected and server side rendering
+      </h1>
+      <p>Welcome User from Django!! {data.email}</p>
       <div>
         <form action={logout}>
-          <button type='submit'>Logout</button>
+
+          <button type='submit' className='flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>Logout</button>
         </form>
       </div>
     </div>
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,11 +1,24 @@
-
+import Link from "next/link";
 
 export default function Home() {
- 
-
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      CSRF : Change Trysss... it so slow indedd.. how to fix
-    </div>
+    <section className='body-font'>
+      <div className='container px-5 py-24 mx-auto'>
+        <div className='flex flex-col text-center w-full mb-12'>
+          <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4'>
+            Next App Router with Django backend session authentication
+          </h1>
+          <p className='lg:w-2/3 mx-auto leading-relaxed text-base'>
+            If you are someone who do not want to leave Django behind but still
+            want to use latest Tech Frontend Javascript NextJs
+          </p>
+          <Link href='/login'>
+            <button className='flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
+              Try to login
+            </button>
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
