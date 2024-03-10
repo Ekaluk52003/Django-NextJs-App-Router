e98a985120758e4a759e5b716b7dcc3b53ff75e3
Ekaluk52003 <ekaluk.p@gmail.com>
2024-03-10 17:37:39 +0700

    done

--- nextjs/src/app/home/page.tsx
+++ nextjs/src/app/home/page.tsx
@@ -1,7 +1,7 @@
 "use server";
 import React, { useState } from "react";
 import { logout, getSession } from "../action";
-
+import Link from "next/link";
 export default async function Home() {
   const data = await getSession();
 
@@ -13,9 +13,21 @@ export default async function Home() {
       <p>Welcome User from Django!! {data.email}</p>
       <div>
         <form action={logout}>
-
-          <button type='submit' className='flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'>Logout</button>
+          <button
+            type='submit'
+            className='flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg'
+          >
+            Logout
+          </button>
         </form>
+      </div>
+      <div>
+        <button
+          type='submit'
+          className='flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-400 rounded text-lg'
+        >
+          <Link href='/'> Home</Link>
+        </button>
       </div>
     </div>
   );
